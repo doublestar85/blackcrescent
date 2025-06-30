e86942b5bfe6170db588d044bcbd325c6112a412
Star <46078392+doublestar85@users.noreply.github.com>
2025-06-30 11:58:09 -0400

    Add VuePress site skeleton

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  base: '/blackcrescent/',
+  title: 'Black Crescent Docs',
+  description: 'Rules for Beyblade X',
+  themeConfig: {
+    sidebar: 'auto',
+    nav: [
+      { text: 'Home', link: '/' }
+    ]
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,4 @@
+# Rules for Beyblade X
+
+Welcome to the Black Crescent documentation. Replace this file with your Markdown export from Google Docs to render it with VuePress.
+
